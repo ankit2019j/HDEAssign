3e5a340ff9336017e7298766b69d565e2c833119
ankit2019j <ankit2019j@gmail.com>
2020-08-23 13:25:41 +0530

    initial

---
+++ missionone.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+
+)
+var Totn int
+var tot []int
+func main() {
+	var outer int
+
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	
+	outer, err := strconv.Atoi(scanner.Text())
+	outer=1
+    if err != nil{
+    	fmt.Println("Error here 1")
+    }
+	
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+    Ttn = outer
+	outerrec(outer)
+}
+func printans(n int, c int) int{
+	if n ==0{
+		return 0
+	}
+    fmt.Println(tot[c])
+    return printans(n-1, c+1)
+}
+func outerrec(n int) int{
+	
+	if n== 0{
+        printans(Ttn, 0)
+    	return 0
+    }
+    var innerloop int
+    var iArr []int
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	innerloop, err := strconv.Atoi(scanner.Text())
+    if err != nil{
+    	fmt.Println("innerloop error")
+    }
+    
+   
+    innerrec(innerloop, iArr)
+
+    
+    return outerrec(n-1)
+
+}
+func innerrec(n int, arr []int) int{
+	
+	if n == 0{
+		e
+		sumofs(arr, (len(arr)-1), 0)
+		
+		return 0
+	}
+	var i int
+	_, err := fmt.Scanf("%d", &i)
+	if err != nil{
+		return 0
+	}
+	
+	arr = append(arr, i)
+	return innerrec(n-1, arr)
+
+}
+
+func sumofs(arr []int, n int, sum int) int{
+	
+
+	if n == -1{
+		
+		tot = append(tot, sum)
+		return -1
+	}
+    if((arr[n] <= 0)&& n>=0){
+
+    	return sumofs(arr, n-1, sum)
+    }else{
+    	
+    	sum+= (arr[n]*arr[n])
+    }
+
+    return sumofs(arr, n-1, sum)
+     
+}
